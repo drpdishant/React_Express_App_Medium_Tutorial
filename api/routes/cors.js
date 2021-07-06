22650d33bb4e9ff0374984a78133610b2e27a271
Dishant Pandya <drpdishant@gmail.com>
2021-07-06 19:54:26 +0530

    update

--- api/routes/cors.js
+++ api/routes/cors.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 //   var origin = req.headers.origin;  
-  res.header('Access-Control-Allow-Origin', global.allowedDomains);
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000','apps.openxcell.dev');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.send('API Enpoint with Cors Allowing Only ' +  global.allowedDomains);
   console.log(JSON.stringify(req.headers));
