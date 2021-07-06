08a8e592ee197785328e50edb2a7d789f2ff3f9d
Dishant Pandya <drpdishant@gmail.com>
2021-07-06 19:26:24 +0530

    update

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-// global.backendUrl = "https://cors-example-api.apps.openxcell.dev"
-global.backendUrl = "http://localhost:9000"
+global.backendUrl = "https://cors-example-api.apps.openxcell.dev"
+// global.backendUrl = "http://localhost:9000"
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
